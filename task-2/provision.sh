698aad2917514bd494054f3f280d8062c0b69ffa
ElSayed Atef <sayedelabd04@gmail.com>
2018-12-23 14:43:14 +0200

    update permission

--- task-2/provision.sh
+++ task-2/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
- sudo apt-get update && apt-get install -y python-dev python-pip
+ sudo apt-get update && sudo apt-get install -y python-dev python-pip
  sudo pip install --upgrade pip
  sudo pip install ansible
  echo "===== finished provision.sh ======="
