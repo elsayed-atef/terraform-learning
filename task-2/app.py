2a544e16a91cd452f5910e7a01fe0f2270f7fcfa
ElSayed Atef <sayedelabd04@gmail.com>
2018-12-23 19:21:38 +0200

    Update app.py

--- task-2/app.py
+++ task-2/app.py
@@ -1,5 +1,8 @@
 import os
+import psycopg2
 from python_terraform import *
+from flask import Flask
+
 tf = Terraform(working_dir='/home/ubuntu')
 
 os.environ['RDS_DB_NAME'] = tf.output('RDS_DB_NAME')
@@ -14,4 +17,26 @@ if 'RDS_DB_NAME' in os.environ:
             'PORT': tf.output('RDS_PORT'),
         }
     }
-print(DATABASES)
+
+
+
+def database():
+     conn = psycopg2.connect(dbname=DATABASES["default"]["NAME"] , host=DATABASES["default"]["HOST"] , password=DATABASES["default"]["PASSWORD"], user=DATABASES["default"]["USER"] , port=DATABASES["default"]["PORT"] )
+     cur = conn.cursor()
+     cur.execute("SELECT num FROM visit  where ID = 1;")
+     value = cur.fetchone()
+     value = int(value[0])
+     cur.execute("UPDATE visit set num = %s where ID = 1",[value +1])
+     conn.commit()
+     cur.close()
+     conn.close()
+     return value
+
+
+app = Flask(__name__)
+@app.route("/")
+def main():
+     value = database()
+     return "Welcome Visitor  " + str (value)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
