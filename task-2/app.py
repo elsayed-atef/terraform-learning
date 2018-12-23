6721268f7b855b40b2aead3716ee4b4a541af7ea
ElSayed Atef <sayedelabd04@gmail.com>
2018-12-23 19:26:24 +0200

    Update app.py

--- task-2/app.py
+++ task-2/app.py
@@ -9,7 +9,6 @@ os.environ['RDS_DB_NAME'] = tf.output('RDS_DB_NAME')
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': tf.output('RDS_DB_NAME'),
             'USER': tf.output('RDS_USERNAME'),
             'PASSWORD': tf.output('RDS_PASSWORD'),
